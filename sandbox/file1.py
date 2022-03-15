dc25f05c7028157d8a2fa26f25fc445c084db191
Mark Orr <mo6xj@virginia.edu>
2022-03-14 22:06:54 -0400

    std

--- sandbox/file1.py
+++ sandbox/file1.py
@@ -6,18 +6,20 @@ import pandas as pd
 #GENERATOR
 N = 1000
 dist_prior = np.random.poisson(75,N)
+#dist_prior = np.random.exponential(15,N)
+#dist_prior = np.round(dist_prior).copy()
 print(dist_prior)
-plt.hist(dist_prior)
+plt.hist(dist_prior,bins=30)
 
 #COMPUTE PROBS FOR EACH EVENT
 dist_prior_event_probs = pd.Series(dist_prior).value_counts()/pd.Series(dist_prior).value_counts().sum()
 dist_prior_event_probs = dist_prior_event_probs.sort_index()
 plt.subplot(2,3,1)
-plt.hist(dist_prior_event_probs)
+plt.hist(dist_prior_event_probs,bins=30)
 plt.subplot(2,3,2)
 plt.plot(dist_prior_event_probs)
 plt.subplot(2,3,3)
-plt.hist(dist_prior)
+plt.hist(dist_prior,bins=30)
 #SUMS TO ONE
 dist_prior_event_probs.sum()
 
@@ -51,7 +53,7 @@ def n_t_tot(x,dist,n):
     #NOTE, THISFU  
     return a
 #EG
-n_t_tot(34,dist_prior_event_probs,N)
+n_t_tot(70,dist_prior_event_probs,N)
 
 def median_of_dist(dist):
     '''
@@ -63,40 +65,27 @@ def median_of_dist(dist):
 #EG
 median_of_dist(dist_prior_event_probs)
 
-
-'''OLD, NOT VALID'''
-def compute_p_t(t,t_total_max,dist):
-    '''
-    This integrates from t to t_total_max to return p(t),
-    over this range, we integrate the following sum
-    dist is the prior
-    '''
-    catch = np.array([])
-    for i in range(t,t_total_max):
-        a = 1/i
-        b = prob_t_tot(i,dist)
-        catch = np.append(catch,a*b)
-
-    return catch.sum()
-'''OLD, NOT VALID'''
-
-'''NEW BETA'''
-dist_prior_event_probs.index
-one_over_ttot = pd.Series(1/dist_prior_event_probs.index,index=dist_prior_event_probs.index)
-
 def compute_p_t(t,dist):
     '''
     t is for current problem
     dist is dist_prior_event_probs, will be pd series.
+    returns a scalar numpy.float64
     '''
     subset = dist_prior_event_probs.iloc[dist_prior_event_probs.index>=t]
-    print('subset',subset)
+    #print('subset',subset)
     one_over_t_total = pd.Series(1/subset.index,index=subset.index) 
-    print('one over t tot',one_over_t_total)
+    #print('one over t tot',one_over_t_total)
     
-    return None
-    
-'''THIS WORKS'''
+    return (subset*one_over_t_total).sum()
+#EG
+compute_p_t(5,dist_prior_event_probs)
+compute_p_t(75,dist_prior_event_probs)
+'''compute_p_t LOGIpC'''
+dist_prior_event_probs.index
+one_over_ttot = pd.Series(1/dist_prior_event_probs.index,index=dist_prior_event_probs.index)
+
+
+
 
 
 '''EXAMPLE
@@ -127,25 +116,15 @@ def compute_posterior(t,dist):
         print('t_total is: ',i)
         #prior
         prior = prob_t_tot(i,dist)
-        likelihood = 1/i 
-        p_t = compute_p_t(t,vect_t_total[-1],dist)
-        
-        catch = np.append(catch,prior*(1/p_t)*likelihood)
+        likelihood = (1/i)*(n_t_tot(i,dist_prior_event_probs,N)) 
+        p_t = compute_p_t(t,dist)
+        likelihood_ratio = (1/i)/p_t
+        print('likelihood, p_t, likelihood_ratio, prior, post: ',likelihood, p_t, likelihood_ratio, prior, likelihood_ratio*prior)
+        catch = np.append(catch,likelihood_ratio*prior)
         
     return catch
 
-
-x = compute_posterior(65,dist_prior_event_probs)
-x = pd.Series(x,index=dist_prior_event_probs.index)
-x.sum()
-x_adj = x*(1/x.sum())
-x_adj.sum()
-median_of_dist(dist_prior_event_probs)
-median_of_dist(x_adj)
-
-
-'''TRY WITHOUT NORMALIZING FACTOR p(t)'''
-def compute_posterior_2(t,dist):
+def compute_posterior_exp(t,dist):
     '''
     dist is the ordered probablility lookup table fro each event t_total
     t_total is what we are computing the distribution over
@@ -156,7 +135,7 @@ def compute_posterior_2(t,dist):
     possible values of t_total. Will fix later.
     '''
     print('t is: ',t)
-    vect_t_total = dist.index
+    vect_t_total = dist.index+1
     print('t_total_range is: ',vect_t_total)
     
     catch = np.array([])
@@ -166,21 +145,49 @@ def compute_posterior_2(t,dist):
         #prior
         prior = prob_t_tot(i,dist)
         likelihood = 1/i 
-        p_t = compute_p_t(t,vect_t_total[-1],dist)
-        
-        catch = np.append(catch,prior*likelihood)
+        p_t = compute_p_t(t,dist)
+        likelihood_ratio = (1/i)/p_t
+        print('likelihood, p_t, likelihood_ratio, prior, post: ',likelihood, p_t, likelihood_ratio,prior,likelihood_ratio*prior)
+        catch = np.append(catch,likelihood_ratio*prior)
         
     return catch
 
-x = compute_posterior_2(70,dist_prior_event_probs)
+
+#RUN ABOVE
+x = compute_posterior(60,dist_prior_event_probs)
 x = pd.Series(x,index=dist_prior_event_probs.index)
 x.sum()
-x_adj = x*60
-x_adj.sum()
+#x_adj = x*(1/x.sum())
+#x_adj.sum()
 median_of_dist(dist_prior_event_probs)
 median_of_dist(x_adj)
+plt.plot(dist_prior_event_probs)
+plt.plot(x_adj)
 
 
 
 
-def compute_decision():
+#HAND HELP
+i=65 #DECISION INPUT
+prob_t_tot(i,dist_prior_event_probs)
+1/i
+compute_p_t(i,dist_prior_event_probs) #FIXED ACROSS ALL T_tot
+
+#DO MY P(t) add to 1?
+catch = ([])
+for i in range(0,103):
+    catch = np.append(catch,compute_p_t(i,dist_prior_event_probs))
+
+catch.sum()
+plt.plot(catch)
+#YES
+
+#DOES MY p(t|t_tot) sum to 1
+catch = ([])
+for i in range(1,103):
+    catch = np.append(catch,1/i)
+    
+catch.sum()
+plt.plot(catch)
+
+#EOF
